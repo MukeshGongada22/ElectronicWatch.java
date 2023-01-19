b879ff34d0390f04ca0d327597618b714ed1b4ff
Gongada Mukesh <mukeshgongada10@gmail.com>
2023-01-19 11:20:01 +0530

    done

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,7 +6,16 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
-
+        int hours=seconds/3600;
+        int min=(seconds%3600)/60;
+        int sec=seconds%60;
+        if(hours==24){
+            hours=0;
+        }
+        if(hours>24){
+            hours=hours%24;
+        }
+        System.out.println(hours +":"+ min +":" +sec);
 
     }
 }
